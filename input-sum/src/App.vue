e7ef11dca597871c3130386eb76a1b4e63dbfb39
benyoss <benyoss4@gmail.com>
2022-12-29 09:56:33 -0600

    (update) App parent to use inputSum component.

---
+++ input-sum/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <InputSum />
+  </div>
+</template>
+
+<script>
+import InputSum from './components/inputSum.vue';
+
+export default {
+  name: 'App',
+  components: {
+    InputSum,
+  },
+};
+</script>
+
+<style lang="css">
+body {
+  background-color: rgb(20, 20, 27);
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+::-webkit-scrollbar {
+    display: none;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
